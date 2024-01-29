962f7eeb7e4414cbb6b923bcf9b8cd0520dd0cdb
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-01-29 15:10:37 -0800

    Added some test code

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BankAccount;
+
+Account MyAccount = new("Yumi Smart");
+MyAccount.Deposit(500);
